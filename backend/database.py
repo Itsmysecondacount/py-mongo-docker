31df6eee5334498c80c25bf923baf58767fe6371
Jorge Castillo <jorge.castillo.torres.a@gmail.com>
2023-07-29 19:25:37 -0500

    [another change]

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from model import Timbre
 # MongoDB driver
 import motor.motor_asyncio
 
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://localhost:27017")
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb://mongo:27017")
 database = client.TodoList
 collection = database.timbre
 
